4155f10e622e368d2e9be7bf80c5d790ebd422cd
Minko Gechev <mgechev@gmail.com>
2012-04-26 21:55:11 +0300

    Files deleted from the repo

--- MandelbrotSet/Model/IFractal.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Drawing;
-
-namespace MandelbrotSet
-{
-    public interface IFractal
-    {
-        void Draw(Bitmap image, double scale, double left, double top, int iterations);
-    }
-}
