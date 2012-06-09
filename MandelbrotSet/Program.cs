ac7d7d33bdc177bd203f2d451db8bbd2efae6372
Minko Gechev <mgechev@gmail.com>
2012-06-09 07:56:00 +0300

    Test
    Test commit via the new windows app

--- MandelbrotSet/Program.cs
+++ MandelbrotSet/Program.cs
@@ -10,7 +10,7 @@ namespace MandelbrotSet
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        [STAThread] 
         static void Main()
         {
             Application.EnableVisualStyles();
